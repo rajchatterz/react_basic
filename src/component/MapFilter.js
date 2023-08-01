b973b4baa49ad5dada0aa9eeace889315f15d412
rajchatterz <iamraj.space@gmail.com>
2023-08-01 19:49:02 +0530

    done with the filter method

--- src/component/MapFilter.js
+++ src/component/MapFilter.js
@@ -2,21 +2,32 @@ import { useEffect, useState } from "react"
 
 function MapFilter(){
     const [data,setData] = useState([])
+    const [number,setNumber] = useState([1,2,3,4,5,6])
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/todos/')
       .then(response => response.json())
       .then(json => setData(json))
+    },[])
+
+    //  Map Function
+//    const mapData = ()=>{
+//     let mapArray = data.map((e)=>e.id+1)
+//     console.log(mapArray)
+//    }
+
+// filter
+
+const mapData = ()=>{
+    let filtter = data.filter((e)=>{
+        let title=e
+        console.log(title.title==="harum ad aperiam quis")
     })
-    // console.log(data)
-    function Map(){
-        let mappArray = data.map((e)=>e.userId+1)
-        // console.log(mappArray)
-    }
-    Map()
+}
+    
     return (
-        <div>{data.map((e)=>{
-            <h1>{e.userId}</h1>
-        })}</div>
+        <div>
+        <button onClick={mapData}>hey</button>
+        </div>
     )
 }
 export default MapFilter
