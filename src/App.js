5a71bccf65585be9cdfa9b8ab130a0adf8f649d9
rajchatterz <iamraj.space@gmail.com>
2023-08-01 20:20:27 +0530

    done with the props

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Increment from './component/Increment';
+import Prop from './component/Prop';
 
 
 function App() {
+  const [name,setName] = useState("Raj")
   return (
     <div>
       <div className="App">
           <h1>List of project</h1>
           {/* <Fetch/> */}
           {/* <MapFilter/> */}
-          <Increment/>
+          {/* <Increment/> */}
+          <Prop name={name}/>
+          <button onClick={()=>setName("Uday")}>Change name</button>
           <h1>hey</h1>
           <p></p>
         </div>
---
+++ src/component/Prop.js
@@ -0,0 +1,9 @@
+function Prop(props){
+    
+    return(
+        <div>
+            {props.name}
+        </div>
+    )
+}
+export default Prop
